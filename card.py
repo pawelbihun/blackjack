5ae03e1f1d96f03f05b1053c5e6c361f2c86e913
Paweł Bihun <pawelbihun@gmail.com>
2023-05-06 14:16:46 +0200

    card class and test

---
+++ card.py
@@ -0,0 +1,32 @@
+"""Class of card with exception"""
+
+
+class InvalidSuit(Exception):
+    """Exception when suit of card is invalid"""
+
+
+class InvalidValue(Exception):
+    """Exception when value of card is invalid"""
+
+
+class Card:
+    """Class represents a single playing card"""
+    possible_values = [str(i) for i in range(2, 11)] + ['J', 'Q', 'K', 'A']
+    possible_suits = {
+        'clubs': '\u2663',
+        'spades': '\u2660',
+        'hearts': '\u2665',
+        'diamonds': '\u2666'
+    }
+
+    def __init__(self, value: str, suit: str) -> None:
+        if value not in self.possible_values:
+            raise InvalidValue('Invalid card value')
+        self.number_or_face = value
+
+        if suit not in self.possible_suits:
+            raise InvalidSuit('Invalid card suit')
+        self.suit = self.possible_suits[suit]
+
+    def __repr__(self) -> str:
+        return f'{self.number_or_face}{self.suit}'
---
+++ card_test.py
@@ -0,0 +1,26 @@
+"""Tests of Card class"""
+import pytest
+from card import Card, InvalidValue, InvalidSuit
+
+
+class TestCard:
+    def test_creation(self):
+        card = Card('A', 'clubs')
+        assert card.number_or_face == 'A'
+        assert card.suit == '♣'
+
+    def test_creation_wrong_value(self):
+        with pytest.raises(InvalidValue) as message:
+            Card('1', 'clubs')
+            assert message == 'Invalid card value'
+
+    def test_creation_wrong_suit(self):
+        with pytest.raises(InvalidSuit) as message:
+            Card('Q', 'Heart')
+            assert message == 'Invalid card suit'
+
+    def test_card_representation(self):
+        assert repr(Card('2', 'clubs')) == '2♣'
+        assert repr(Card('2', 'spades')) == '2♠'
+        assert repr(Card('2', 'hearts')) == '2♥'
+        assert repr(Card('2', 'diamonds')) == '2♦'
